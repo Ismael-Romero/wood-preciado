512f391376fc4075a2f27e766ed419c446cfbcc6
Ismael Romero <76606976+Ismael-Romero@users.noreply.github.com>
2024-06-12 21:00:54 -0600

    Update dashboard.tsx

--- wood-desktop/src/pages/dashboard/dashboard.tsx
+++ wood-desktop/src/pages/dashboard/dashboard.tsx
@@ -1,10 +1,60 @@
-
+import {Card, Layout, Statistic} from "antd";
+import {ArrowDownOutlined, ArrowUpOutlined} from "@ant-design/icons";
 
 
 export const DashboardPage = () => {
     return (
-        <div>
-            Dashboard
-        </div>
+        <Layout className={"bg-transparent"}>
+            <Layout className={"w-full flex flex-row flex-wrap gap-4"}>
+                <Card style={{width: "auto"}}>
+                    <Statistic title="Ventas acumuladas"
+                               valueStyle={{
+                                   color: 'green',
+                               }}
+                               precision={2}
+                               prefix={<ArrowUpOutlined />}
+                               suffix={"MXN"}
+                               value={120} />
+                </Card>
+                <Card style={{width: 215}}>
+                    <Statistic title="Ventas pendientes"
+                               valueStyle={{
+                                   color: 'blue',
+                               }}
+                               precision={2}
+                               prefix={<ArrowDownOutlined />}
+                               value={5} />
+                </Card>
+                <Card style={{width: 215}}>
+                    <Statistic title="Ventas confirmadas"
+                               valueStyle={{
+                                   color: 'blue',
+                               }}
+                               precision={2}
+                               prefix={<ArrowDownOutlined />}
+                               value={12} />
+                </Card>
+                <Card style={{width: 215}}>
+                    <Statistic title="Ventas entregadas"
+                               valueStyle={{
+                                   color: 'blue',
+                               }}
+                               precision={2}
+                               prefix={<ArrowDownOutlined />}
+                               value={12} />
+                </Card>
+                <Card style={{width: 215}}>
+                    <Statistic title="Salidas de inventario"
+                               valueStyle={{
+                                   color: 'red',
+                               }}
+                               precision={2}
+                               prefix={<ArrowDownOutlined />}
+                               value={120} />
+                </Card>
+            </Layout>
+
+
+        </Layout>
     );
 };
